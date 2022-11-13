65a5148aa9f3374326f50f76aa474fd842f5cba3
Usshel <pawelopadczuk@gmail.com>
2022-11-13 01:34:06 +0100

    feat: title added feat: title prop added feat: List of Employees added feat: employees email added feat: Service created feat: faces and list of employees connected with service feat: new data model for employees feat: new data model, service, component - project list
    Employees faces: images with names added and setted up horizontally

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
 import {EmployeesFacesComponentModule} from "./ui/employees-faces/employees-faces.component-module";
 import {EmployeeServiceModule} from "./services/employee.service-module";
+import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
+import {ProjectServiceModule} from "./services/project.service-module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +18,8 @@ import {EmployeeServiceModule} from "./services/employee.service-module";
     EmployeeListComponentModule,
     EmployeesFacesComponentModule,
     EmployeeServiceModule,
+    ProjectListComponentModule,
+    ProjectServiceModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
