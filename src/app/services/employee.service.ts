138de874c228d356239baa503d463e3db0f67ff2
Usshel <pawelopadczuk@gmail.com>
2022-11-16 03:11:33 +0100

    feat: Delete method created in Service feat: remove method created in Employee-list feat: Remove Button linked with remove method created

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map, Observable} from "rxjs";
-import {EmployeeModel} from "../model/employee.model";
-import {ApiResponse} from "./api.response";
-import {EmployeeResponse} from "./employee.response";
-import {CreateEmployeeModel} from "../model/create-employee.model";
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { EmployeeModel } from '../model/employee.model';
+import { ApiResponse } from './api.response';
+import { EmployeeResponse } from './employee.response';
+import { CreateEmployeeModel } from '../model/create-employee.model';
 
 @Injectable()
 export class EmployeeService {
@@ -16,7 +17,7 @@ export class EmployeeService {
       'https://dummy.restapiexample.com/api/v1/employees',
     ).pipe(
       map((response: ApiResponse<EmployeeResponse[]>) => {
-        return response.data.map((employeeResponse: EmployeeResponse) =>{
+        return response.data.map((employeeResponse: EmployeeResponse) => {
           return {
             employeeId: employeeResponse.id,
             employeeName: employeeResponse.employee_name,
@@ -29,10 +30,12 @@ export class EmployeeService {
   }
 
   create(employee: CreateEmployeeModel): Observable<any> {
-    return this._httpClient.post(	'https://dummy.restapiexample.com/api/v1/create', employee);
+    return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create', employee);
   }
 
-
+  delete(id: string): Observable<void> {
+    return this._httpClient.delete( 'https://dummy.restapiexample.com/api/v1/delete/' + id).pipe(map(_ => void 0));
+  }
 
 
 } //end of service
--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import {Observable} from "rxjs";
 import {EmployeeModel} from "../../model/employee.model";
 import {EmployeeService} from "../../services/employee.service";
@@ -14,6 +13,9 @@ export class EmployeeListComponent {
   data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();
   constructor(private _employeeService: EmployeeService) {
 
+  }
+  remove(id: string){
+  this._employeeService.delete(id).subscribe();
   }
 
 }
